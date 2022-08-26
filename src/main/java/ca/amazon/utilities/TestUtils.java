37a8dbbc8752e6cb612313160db6502aaaf8147e
SHIVAMPARIKH2021 <96095609+SHIVAMPARIKH2021@users.noreply.github.com>
2022-08-26 10:19:26 -0400

    Removed Test Pages To Reduce Overlaping Of Testing Activities

--- src/main/java/ca/amazon/utilities/TestUtils.java
+++ src/main/java/ca/amazon/utilities/TestUtils.java
@@ -1,25 +1,9 @@
 package ca.amazon.utilities;
 
 
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.Select;
-
 import ca.amazon.basepackage.BaseTest;
 
 public class TestUtils extends BaseTest {
 	
-private static Actions action;
-private static Select select;
 
-
-	public Actions action() {
-		action = new Actions(driver);
-		return action;
-	}
-	
-	public Select select() {
-		select = new Select((WebElement) driver);
-		return select;
-	}
 }
