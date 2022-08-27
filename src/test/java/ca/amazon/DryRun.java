bc9c92dc9ab3efc3712382b50d4bcf96f1d7d3dc
SHIVAMPARIKH2021 <96095609+SHIVAMPARIKH2021@users.noreply.github.com>
2022-08-27 02:05:49 -0400

    Dpendencies are updated

--- src/test/java/ca/amazon/DryRun.java
+++ src/test/java/ca/amazon/DryRun.java
@@ -1,10 +1,21 @@
 package ca.amazon;
 
-public class DryRun {
+import org.testng.annotations.Test;
+
+import ca.amazon.basepackage.BaseTest;
+
+public class DryRun extends BaseTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("This is the Dry Running Class of the project");
 	}
-
+@Test
+public void primarytest() {
+	/*
+	 * String a = "Shivam"; String b = "Shiva"; if(a!=b){ Assert.assertEquals(a,b);
+	 * System.out.println("Dry Run is Passed"); } else {
+	 * System.out.println("Dry Run is Failed"); }
+	 */
+}
 }
