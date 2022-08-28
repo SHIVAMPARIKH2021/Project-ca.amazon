9cd0589af6742a5096acf3a319cb9bd61b5cff2f
SHIVAMPARIKH2021 <96095609+SHIVAMPARIKH2021@users.noreply.github.com>
2022-08-28 08:13:33 -0400

    Triggering the First Build with First Test Case

--- src/main/java/ca/amazon/pages/AmazonSignIn.java
+++ src/main/java/ca/amazon/pages/AmazonSignIn.java
@@ -1,20 +1,31 @@
 package ca.amazon.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import ca.amazon.basepackage.BaseTest;
 
 public class AmazonSignIn extends BaseTest {
-
+    static Actions action;
+    
+    @FindBy(id = "nav-link-accountList-nav-line-1")
+    private static WebElement SignInButton;
+    
 	@FindBy(id = "ap_email")
 	private static WebElement EmailOrMobile;
 	
 	@FindBy(xpath = "//input[@tabindex='5'and@id='continue']")
 	private static WebElement ContinueButton;
 	
-	AmazonSignIn(){
+	protected AmazonSignIn(){
 		PageFactory.initElements(driver,AmazonSignIn.class);
+	}
+	
+	public static void SignIn() {
+		action.moveToElement(driver.findElement(By.id("SignInButton"))).build().perform();
+		SignInButton.click();
 	}
 	
 	public static void Email(String loginid) {
---
+++ src/test/java/ca/amazon/TestAmazonSignIn.java
@@ -0,0 +1,33 @@
+package ca.amazon;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import ca.amazon.pages.AmazonSignIn;
+
+public class TestAmazonSignIn extends AmazonSignIn {
+	
+	TestAmazonSignIn() {
+		super();
+	}
+	
+	@BeforeTest
+	public void StartTest() {
+		initiate();
+	}
+	
+	@Test
+	public void SigninButtonTest() {
+		SignIn();
+		String title = driver.getTitle().toString();
+		Assert.assertEquals(title, "Amazon Sign In");
+	}
+	
+	@AfterTest
+	public void EndTest() {
+		quitbrowser();
+	}
+
+}
