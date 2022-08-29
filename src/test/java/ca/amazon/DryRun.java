0d37310d5b2444c72c7cfc12016c4082df1fef5e
SHIVAMPARIKH2021 <96095609+SHIVAMPARIKH2021@users.noreply.github.com>
2022-08-28 23:18:36 -0400

    Adding a Test Case

--- src/test/java/ca/amazon/DryRun.java
+++ src/test/java/ca/amazon/DryRun.java
@@ -1,6 +1,10 @@
 package ca.amazon;
 
 import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
 import ca.amazon.basepackage.BaseTest;
@@ -17,6 +21,14 @@ public void primarytest() {
 	  System.out.println("Dry Run is Failed");
 	}	 
 	  quitbrowser();
+}
+@Test
+public void secondtest() {
+	initiate();
+	Actions action = new Actions(driver);
+	action.moveToElement(driver.findElement(By.xpath("//span[text()='Hello, Sign in']"))).build().perform();
+	action.moveToElement(driver.findElement(By.xpath("//span[text()='Hello, Sign in']"))).build().perform();
+	driver.findElement(By.xpath("//span[text()='Sign in']")).click();
 }
 
 }
